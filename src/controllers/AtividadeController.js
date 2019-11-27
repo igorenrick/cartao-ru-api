e7d87c597d4ab6c1bcce957bf4ba4dbad9498a42
Igor Enrick <igorenrickc@gmail.com>
2019-11-27 16:31:47 -0300

    Adiciona list de reloads

--- src/controllers/AtividadeController.js
+++ src/controllers/AtividadeController.js
@@ -1,5 +1,6 @@
 const Atividade = require('../models/AtividadeModel')
 const Uso = require('../models/UsoModel')
+const Recarga = require('../models/RecargaModel')
 
 module.exports = {
     async list(req, res, next) {
@@ -19,9 +20,24 @@ module.exports = {
             console.log('Atividade: ' + atividade)
 
             const usos = await Uso.find({dono: atividade.dono}).sort({'data': -1}).limit(3)
-            console.log('Uso[0]: ' + usos)
+            console.log('Uso: ' + usos)
 
             return res.json(usos)
+        } catch (error) {
+            res.status(400).send(error)
+        }
+    },
+
+    async listreloads(req, res, next) {
+        try {
+            const _id = req.body
+            const atividade = await Atividade.findOne({_id})
+            console.log('Atividade: ' + atividade)
+
+            const recargas = await Recarga.find({dono: atividade.dono}).sort({'data': -1}).limit(3)
+            console.log('Recargas: ' + recargas)
+
+            return res.json(recargas)
         } catch (error) {
             res.status(400).send(error)
         }
